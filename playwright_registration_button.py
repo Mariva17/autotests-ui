49f37db8427d3955e3f38bbc596b68eff2a8f999
Mariva17 <margo.ivanova.dd@gmail.com>
2025-09-15 13:57:30 +0200

    Углубление в Playwright

--- playwright_registration_button.py
+++ playwright_registration_button.py
@@ -1,10 +1,15 @@
 from playwright.sync_api import sync_playwright, expect
 
+from playwright_registration import registration_button
+
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
 
-    page.goto(" https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    expect(registration_button).to_be_disabled()
 
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
@@ -16,11 +21,7 @@ with sync_playwright() as playwright:
     password_input.fill('password')
 
     registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
-
-    header_dashboard = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(header_dashboard).to_be_visible()
-    expect(header_dashboard).to_have_text('Dashboard')
+    expect(registration_button).to_be_enabled()
 
 
 
