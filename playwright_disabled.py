f769210c9bae67dc32b81bb4a18c1b3faf3883f0
Mariva17 <margo.ivanova.dd@gmail.com>
2025-09-15 13:32:30 +0200

    Углубление в Playwright

---
+++ playwright_disabled.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright, expect
+
+from playwright_authorization import login_button
+from playwright_registration import browser
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+    login_button = page.get_by_test_id('login-page-login-button')
+
+    expect(login_button).to_be_disabled()
+
+
+    page.wait_for_timeout(5000)
